bce83376b85bdf1a13bcdb05c514fe2ae1e35646
Miguel Branco <miguel@raw-labs.com>
2025-05-17 21:47:01 +0200

    Fix type definition to include all fields required

--- src/raw/endpoints/types.py
+++ src/raw/endpoints/types.py
@@ -14,17 +14,32 @@ class TestDefinition(TypedDict):
     arguments: List[TestArgument]
     result: Optional[object]
 
+class TypeDefinition(TypedDict):
+    type: str
+    format: Optional[str]  # email, uri, date, time, date-time, duration, timestamp
+    minLength: Optional[int]
+    maxLength: Optional[int]
+    minItems: Optional[int]
+    maxItems: Optional[int]
+    items: Optional['TypeDefinition']
+    properties: Optional[dict[str, 'TypeDefinition']]
+    required: Optional[List[str]]
+
 class ParamDefinition(TypedDict):
     name: str
     type: str
-    description: Optional[str]
-    required: Optional[bool]
+    description: str
     default: Optional[object]
-
-class TypeDefinition(TypedDict):
-    type: str
-    items: Optional['TypeDefinition']
-    properties: Optional[dict[str, 'TypeDefinition']]
+    examples: Optional[List[object]]
+    enum: Optional[List[object]]
+    # Type constraints inherited from TypeDefinition
+    format: Optional[str]
+    minLength: Optional[int]
+    maxLength: Optional[int]
+    minItems: Optional[int]
+    maxItems: Optional[int]
+    items: Optional[TypeDefinition]
+    properties: Optional[dict[str, TypeDefinition]]
     required: Optional[List[str]]
 
 class ToolDefinition(TypedDict):
