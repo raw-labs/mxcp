475f25d22333119fd705845bd5444f3039fcb1ec
torcato <cesar.matos@gmail.com>
2025-05-27 10:54:44 +0200

    Adding example files

---
+++ examples/covid_owid/models/covid_data.sql
@@ -0,0 +1,4 @@
+{{ config(materialized='table') }}
+
+select *
+from read_csv_auto('https://github.com/owid/covid-19-data/raw/master/public/data/owid-covid-data.csv')
---
+++ examples/covid_owid/models/hospitalizations.sql
@@ -0,0 +1,5 @@
+{{ config(materialized='table') }}
+
+select *
+
+from read_csv_auto('https://github.com/owid/covid-19-data/raw/master/public/data/hospitalizations/covid-hospitalizations.csv')
